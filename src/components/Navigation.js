35f22a3113c2db59f7ccbe8127bd51d8a0324ce4
Eli Sacks <bastlifa@gmail.com>
2019-08-10 17:59:15 -0400

    Refactored Nav and ShopCart. MVP

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CartContent } from '../contexts/CartContext';
 
-const Navigation = props => {
+const Navigation = _ => {
+	const cart = useContext(CartContent)
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
 			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+				Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
